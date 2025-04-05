aa6f74956b19ee1ce4f37b2386bf1e41baa66f25
oyarzunaspee <oyarzunaspee.work@gmail.com>
2025-04-04 22:27:35 -0300

    Update settings.py

--- ficrec/settings.py
+++ ficrec/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 from datetime import timedelta
 import os
 import dj_database_url
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_KEY')
+SECRET_KEY = env('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "almondluu.pythonanywhere.com"]
 
