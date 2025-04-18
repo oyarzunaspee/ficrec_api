01a4083f2734c2aecb9e105e24e41eb5e3de3bca
oyarzunaspee <oyarzunaspee.work@gmail.com>
2025-04-18 00:05:28 -0400

    Add max_length constraint to bio field in Reader model

--- authentication/models.py
+++ authentication/models.py
@@ -6,7 +6,7 @@ class Reader(models.Model):
     uid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_reader')
     avatar = models.TextField(blank=True, null=True)
-    bio = models.TextField(blank=True, null=True)
+    bio = models.TextField(blank=True, null=True, max_length=100)
 
     def __str__(self):
         return self.user.username
