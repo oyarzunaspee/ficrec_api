f2e3db0f193ba07a3a3d1982246b3a70121b1273
oyarzunaspee <oyarzunaspee.work@gmail.com>
2025-05-09 15:40:54 -0400

    Enhance CustomPagination to include next page calculation and set default page size to 10

--- utils/pagination.py
+++ utils/pagination.py
@@ -2,16 +2,23 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class CustomPagination(PageNumberPagination):
+    page_size = 10
+    
     def get_page_number(self, request, paginator):
-        page_number = request.query_params.get(self.page_query_param) or 1
-        self.current = page_number
+        page_number = request.query_params.get("page") or 1
+        self.current = int(page_number)
+        if self.current < paginator.num_pages:
+            self.next = self.current + 1
+        else:
+            self.next = None
         if page_number in self.last_page_strings:
             page_number = paginator.num_pages
         return page_number
     
     def get_paginated_response(self, data):
         return Response({
-            "pages": self.page.paginator.count,
+            "pages": self.page.paginator.num_pages,
             "current": self.current,
+            "next": self.next,
             "results": data,
         })
