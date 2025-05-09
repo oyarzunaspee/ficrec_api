04bf60f0f77c5caccd924b70f566318b9a10472d
oyarzunaspee <oyarzunaspee.work@gmail.com>
2025-05-09 15:30:54 -0400

    Change default pagination class to CustomPagination

--- ficrec/settings.py
+++ ficrec/settings.py
@@ -65,7 +65,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'utils.pagination.CustomPagination',
     'PAGE_SIZE': 10
 }
 
