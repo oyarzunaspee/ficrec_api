aa34c140e3d21e97a17d40183d15b5b29e578f10
oyarzunaspee <oyarzunaspee.work@gmail.com>
2025-05-25 18:11:00 -0400

    Add CORS support by including django-cors-headers in settings and requirements

--- ficrec/settings.py
+++ ficrec/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'authentication',
@@ -77,6 +78,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
 ]
 
 
