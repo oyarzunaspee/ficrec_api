2454007d487bde769278ca7b60c7bae7c920e2c7
oyarzunaspee <oyarzunaspee.work@gmail.com>
2025-06-09 23:46:25 -0400

    Add notes field to RecSerializer and ensure django-cors-headers is included in requirements

--- utils/serializers.py
+++ utils/serializers.py
@@ -1,6 +1,6 @@
 from utils.fields import TagsField, CurrentModel
 from user_profile.models import Rec, Collection
-from rest_framework.serializers import ModelSerializer, HiddenField, SerializerMethodField
+from rest_framework.serializers import ModelSerializer, HiddenField, SerializerMethodField, CharField
 
 class RecSerializer(ModelSerializer):
     author = TagsField()
@@ -10,6 +10,7 @@ class RecSerializer(ModelSerializer):
     characters = TagsField()
     tags = TagsField()
     collection = HiddenField(default=CurrentModel())
+    notes = CharField(allow_blank=True, required=False)
 
     class Meta:
         model = Rec
